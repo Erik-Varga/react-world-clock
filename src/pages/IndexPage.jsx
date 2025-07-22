5aae08adb67ccb74c7e6bb59028708a65821c246
Erik Varga <viglew@gmail.com>
2025-07-22 15:04:58 -0400

    updated header

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -5,7 +5,7 @@ import WorldClock from '../components/WorldClock'
 const IndexPage = () => {
 
   return (
-    <div className='w-full md:w-1/2 text-center text-sm border border-gray-500 p-5 rounded z-10 bg-gray-100 dark:bg-gray-800'>
+    <div className='w-full md:w-1/2 text-center text-sm border border-gray-500 p-5 rounded z-10 bg-gray-50 dark:bg-gray-900'>
 
       <div className="flex justify-center items-center text-2xl mb-3">
         <Title text1={'Time'} text2={'Zones'} />
